be9bd9455ed973d01786e8baa4d60a1dca0d0343
jjajja91 <jjajja91@hanmail.net>
2020-06-29 15:22:17 +0900

    description commit

--- src/main/java/alz/classes/controller/ClassPageController.java
+++ src/main/java/alz/classes/controller/ClassPageController.java
@@ -1,5 +1,27 @@
 package alz.classes.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import alz.classes.service.ClassService;
+
+@Controller
+@RequestMapping("/class")
 public class ClassPageController {
+	
+	private ClassService classService;
+	
+	@Autowired
+	public ClassPageController(ClassService classService) {
+		this.classService = classService;
+	}
+	
+	@GetMapping("/list")
+	public void list(Model model){
+		model.addAttribute("list", classService.readAll());
+	}
 
 }
