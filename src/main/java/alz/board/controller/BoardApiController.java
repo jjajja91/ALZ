def857e810992435408d3e16b209ce79293712c2
findh <findh@LAPTOP-U2EIVEB9>
2020-07-02 20:32:20 +0900

    페이지, 글읽기 완료(버그있을수있음)

--- src/main/java/alz/board/controller/BoardApiController.java
+++ src/main/java/alz/board/controller/BoardApiController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -47,6 +48,42 @@ public class BoardApiController {
 		List<BoardDTO> boards = boardService.readAll();
 		
 		return ResponseEntity.status(HttpStatus.OK).body(boards);
+	}
+	
+	// 검색 결과 글목록
+	@GetMapping(value = {"/{pageNum}/{amount}/{type}/{keyword}", "/{pageNum}/{amount}/{type}"},
+			produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public ResponseEntity<?> readAll(@PathVariable String type, 
+									@PathVariable(name="keyword", required = false) String keyword,
+									@PathVariable Integer pageNum, @PathVariable Integer amount) {
+		BoardCriteria cri = new BoardCriteria();
+		
+		cri.setKeyword(keyword);
+		cri.setType(type);
+		cri.setPageNum(pageNum);
+		cri.setAmount(amount);
+		
+		List<BoardDTO> boards = boardService.readAll(cri);
+		
+		return ResponseEntity.status(HttpStatus.OK).body(boards);
+	}
+	
+	// 글 수 카운트
+	@GetMapping(value = {"/type/{type}/keyword/{keyword}", "type/{type}"},
+			produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public int readAll(@PathVariable String type, 
+					@PathVariable(name="keyword", required = false) String keyword) {
+
+		BoardCriteria cri = new BoardCriteria();
+		
+		if(keyword!=null) {
+			cri.setKeyword(keyword);
+			cri.setType(type);
+		}
+		
+		int total = boardService.getTotal(cri);
+		
+		return total;
 	}
 	
 	@PutMapping("/{id}")
--- src/main/webapp/WEB-INF/views/includes/header.jsp
+++ src/main/webapp/WEB-INF/views/includes/header.jsp
@@ -9,5 +9,6 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
 </head>
 <body>
