c17a23cb5528daf25e3f2375b9785fd58bd316f2
bpuuq <bpuuq@>
2020-07-08 10:27:35 +0900

    test

---
+++ src/test/java/alz/board/service/BoardServiceTests.java
@@ -0,0 +1,41 @@
+package alz.board.service;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import alz.board.domain.BoardCriteria;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class BoardServiceTests {
+
+   @Setter(onMethod_ = {@Autowired})
+   private BoardService boardService;
+   
+   @Test
+   public void testSelect() {
+      
+      boardService.readAll(new BoardCriteria(1, 10)).forEach(board -> log.info(board));
+      
+   }
+   
+   
+   @Test
+   public void testSelectById() {
+	   
+	 log.info(boardService.readById(12L)); 
+   }
+   @Test
+   public void testExist() {
+      log.info(boardService);
+      assertNotNull(boardService);
+   }
+}
