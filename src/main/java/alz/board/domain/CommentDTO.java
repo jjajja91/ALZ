e57e531107f6e40f828ab266b00483bff50bb0b8
findh <findh@192.168.219.103>
2020-07-12 16:44:47 +0900

    빼먹은거 다시 올림

--- src/main/java/alz/board/domain/CommentDTO.java
+++ src/main/java/alz/board/domain/CommentDTO.java
@@ -2,15 +2,11 @@ package alz.board.domain;
 
 import java.util.Date;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 import lombok.experimental.Accessors;
 
 
@@ -20,12 +16,9 @@ import lombok.experimental.Accessors;
 @Accessors(chain=true)
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 public class CommentDTO {
 	
 	private Long id;
-	@NotBlank(message = "글 내용을 입력해주세요")
-	@Size(min=1, max=2000, message="내용은 2000자 이내로 입력해주세요")
 	private String content;
 	private Date writtenAt;
 	private Date updatedAt;
