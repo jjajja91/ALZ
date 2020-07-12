6b7f6ab5f957e2859d89176e9b2cb6c48a5679a2
jjajja91 <jjajja91@hanmail.net>
2020-07-13 00:21:42 +0900

    board controller error fixed commit

--- src/main/java/alz/board/controller/BoardPageController.java
+++ src/main/java/alz/board/controller/BoardPageController.java
@@ -5,15 +5,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-import javax.validation.Valid;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
