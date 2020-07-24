55b17fe491950c5aaa40afe2855d40e4b705a789
findh <findh@LAPTOP-U2EIVEB9>
2020-07-24 18:04:42 +0900

    읽기 완료(한줄평 추가는 아직..)

---
+++ src/main/java/alz/lesson/domain/CurriculumSubjectDTO.java
@@ -0,0 +1,29 @@
+package alz.lesson.domain;
+
+import java.sql.Date;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@Builder
+@Accessors(chain=true)
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class CurriculumSubjectDTO {
+	
+	private Long subjectId;
+	private Long curriculumOrder;
+	private String subject;
+	
+	private List<CurriculumDetailDTO> detailList;
+
+}
